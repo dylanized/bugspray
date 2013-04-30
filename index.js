3f2b10eaf2231371afd6ef6508e1c83a7fb69ccf
dylan hassinger <d@dylanized.com>
2013-04-30 01:35:06 -0500

    added export function

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
 // Bugspray
 
-edt = require('express-debug');
-error-face = require('errorface');
+module.exports = function(app) {
 
-// launch express debug tab
-if (process.argv[2] == "edt" || 
-	process.argv[2] == "--edt" ||
-	process.argv[2] == "debug" ||
-	process.argv[2] == "--debug" ||
-	process.argv[2] == "--debug-brk") {
-		app.use(edt(app, {}))
+	edt = require('express-debug');
+	errorface = require('errorface');
+	
+	// launch express debug tab
+	if (process.argv[2] == "edt" || 
+		process.argv[2] == "--edt" ||
+		process.argv[2] == "debug" ||
+		process.argv[2] == "--debug" ||
+		process.argv[2] == "--debug-brk") {
+			app.use(edt(app, {}))
+	}
+	
+	// launch special error page
+	app.use(errorface.errorHandler());
+
 }
-
-// launch special error page
-app.use(errorface.errorHandler());
